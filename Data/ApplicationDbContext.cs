cb27491682be8227dc0c19bb3d18fc7e87fe1fa9
malakdaraghmeh <malakdaraghmeh23@gmail.com>
2024-08-11 20:22:27 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task2_crud.Models;
+
+namespace task2_crud.Data
+{
+    internal class ApplicationDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;Database=ASP9_EF_crud;Trusted_Connection=True;TrustserverCertificate=True");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using task2_crud.Data;
+using task2_crud.Models;
+
+namespace task2_crud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+            //create
+            
+            Product p= new Product() { Name="ali",Price=2000 };
+            dbContext.Products.Add(p);
+            Order o = new Order() {  Address = "Nablus",CreateAt=new DateTime(2024,8,9,22,40,27,210) };
+            dbContext.Orders.Add(o);
+            //read
+            var products = dbContext.Products.ToList();
+            foreach(var product in products)
+            {
+                Console.WriteLine(product.Name);
+            }
+            var orders = dbContext.Orders.ToList();
+            foreach (var order in orders)
+            {
+                Console.WriteLine(order.Address);
+            }
+            //update
+            var pro = dbContext.Products.First(p => p.Id == 3);
+            pro.Name = "Tariq";
+           
+            var ord = dbContext.Orders.First(o => o.Id == 1);
+            ord.CreateAt = new DateTime(2024, 8, 17, 23, 12, 10, 210);
+            
+            //remove
+           var prod = dbContext.Products.First(p => p.Id == 2);
+           dbContext.Products.Remove(prod);
+
+           var ore = dbContext.Orders.First(ord => ord.Id == 3);
+           dbContext.Orders.Remove(ore);
+
+           
+            dbContext.SaveChanges();
+          
+           
+            Console.WriteLine("done");
+
+        }
+    }
+}
